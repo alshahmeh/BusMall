03b709f0c178f011cd154ee12a97293d7e061c19
Ali Alshahmeh <alshahmeh2@gmail.com>
2021-05-08 21:52:02 +0300

    I added local storage

--- app.js
+++ app.js
@@ -1,170 +1,119 @@
 'use strict';
+const section=document.getElementById('section');
+const button=document.getElementById('button');
+const leftImage=document.getElementById('leftImage');
+const middleImage=document.getElementById('middleImage');
+const rightImage=document.getElementById('rightImage');
+const ul=document.getElementById('ul');
+let display=0;
+let randomArray=[];
 let imagesArray = [
-  'bag.jpg',
-  'banana.jpg',
-  'bathroom.jpg',
-  'boots.jpg',
-  'breakfast.jpg',
-  'bubblegum.jpg',
-  'chair.jpg',
-  'cthulhu.jpg',
-  'dog-duck.jpg',
-  'dragon.jpg',
-  'pen.jpg',
-  'pet-sweep.jpg',
-  'scissors.jpg',
-  'shark.jpg',
-  'sweep.png',
-  'tauntaun.jpg',
-  'unicorn.jpg',
-  'usb.gif',
-  'water-can.jpg',
-  'wine-glass.jpg'
-];
-const showResults = document.getElementById('button');
-const resultContainer = document.getElementById('resultContainer');
-const rightImage = document.getElementById('rightImage');
-const middleImage = document.getElementById('middleImage');
-const leftImage = document.getElementById('leftImage');
-const imageSection = document.getElementById('imageSection');
-let clickNumber=0;
-let rightImageIndex=0;
-let leftImageIndex=0;
-let middleImageIndex=0;
-Images.lastShown=[];
-function Images(name) {
-  this.name = name.split('.')[0];
-  this.img = `./img/${name}`;
-  this.shown = 0;
-  this.clicks = 0;
-  Images.all.push(this);
-
-}
-Images.all=[];
-for (let i = 0; i < imagesArray.length; i++) {
-  new Images(imagesArray[i]);
-}
-
-imageSection.addEventListener('click', eventHandler);
-function eventHandler(e) {
-  if ((e.target.id === 'leftImage' || e.target.id === 'rigthImage' || e.target.id === 'middleImage') && clickNumber < 25) {
-
-    if (e.target.id === 'leftImage') { Images.all[leftImageIndex].clicks++; }
-    if (e.target.id === 'rightImage') { Images.all[rightImageIndex].clicks++; }
-    if (e.target.id === 'middleImage') { Images.all[middleImageIndex].clicks++; }
-    clickNumber++;
-    renderImages();
+    'bag',
+    'banana',
+    'bathroom',
+    'boots',
+    'breakfast',
+    'bubblegum',
+    'chair',
+    'cthulhu',
+    'dog-duck',
+    'dragon',
+    'pen',
+    'pet-sweep',
+    'scissors',
+    'shark',
+    'sweep',
+    'tauntaun',
+    'unicorn',
+    'usb',
+    'water-can',
+    'wine-glass'
+  ];
+  Images.allImages=[];
+  function randomNumber(){return Math.floor(Math.random()*(imagesArray.length-1));}
+  function Images(name){
+this.shown=0;
+this.click=0;
+this.name=name;
+Images.allImages.push(this);
   }
-  else{renderChart();getData();}
+for(let i=0;i<imagesArray.length;i++){
+    new Images(imagesArray[i]);
 }
-saveData();
-function saveData(){localStorage.setItem('key',JSON.stringify(Images.all));
-  localStorage.setItem('key1',Images.all.clicks);
-  localStorage.setItem('key2',Images.all.shown);
+
+let getData=JSON.parse(localStorage.getItem('images'));
+if(getData){
+    for(let z=0;z<getData.length;z++){
+        Images.allImages[z].name=getData[z].name;
+        Images.allImages[z].click=getData[z].click;
+        Images.allImages[z].shown=getData[z].shown;}}
+/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */
+let leftRandom=randomNumber();
+let middleRandom=randomNumber();
+let rightRandom=randomNumber();
+  function renderImages(){
+while(leftRandom===middleRandom||leftRandom===rightRandom||middleRandom===rightRandom||randomArray.includes(leftRandom)||randomArray.includes(middleRandom)||randomArray.includes(rightRandom)){
+    leftRandom=randomNumber();
+    middleRandom=randomNumber();
+    rightRandom=randomNumber();}
+    randomArray=[];
+    randomArray.push(leftRandom);
+    randomArray.push(middleRandom);
+    randomArray.push(rightRandom);
+leftImage.src=`./img/${imagesArray[leftRandom]}.jpg`;
+Images.allImages[leftRandom].shown++;
+middleImage.src=`./img/${imagesArray[middleRandom]}.jpg`;
+Images.allImages[middleRandom].shown++;
+rightImage.src=`./img/${imagesArray[rightRandom]}.jpg`;
+Images.allImages[rightRandom].shown++;
 }
-function renderImages() {
-  let leftIndex =Math.floor( Math.random() * imagesArray.length);
-  let middleIndex=Math.floor( Math.random() * imagesArray.length);
-  let rightIndex=Math.floor( Math.random() * imagesArray.length);
-  while( leftIndex === middleIndex
-    || middleIndex === rightIndex
-    || leftIndex === rightIndex
-    || Images.lastShown.includes(leftIndex)
-    || Images.lastShown.includes(middleIndex)
-    || Images.lastShown.includes(rightIndex)){
-    leftIndex= Math.floor(Math.random()* imagesArray.length);
-    middleIndex= Math.floor(Math.random()*imagesArray.length);
-    rightIndex= Math.floor(Math.random()* imagesArray.length);
-  }
-
-  rightImage.src = Images.all[rightIndex].img;
-  middleImage.src = Images.all[middleIndex].img;
-  leftImage.src = Images.all[leftIndex].img;
-
-  rightImageIndex = rightIndex;
-  middleImageIndex = middleIndex;
-  leftImageIndex = leftIndex;
-
-  Images.all[leftIndex].shown++;
-  Images.all[rightIndex].shown++;
-  Images.all[middleIndex].shown++;
-
-
-  Images.lastShown=[];
-  Images.lastShown.push(leftIndex);
-  Images.lastShown.push(middleIndex);
-  Images.lastShown.push(rightIndex);
-}
-showResults.removeEventListener('click', viewResults);
-
-
 renderImages();
-
-showResults.addEventListener('click', viewResults);
-
-function viewResults( ) {
-  let ul = document.createElement('ul');
-  resultContainer.appendChild(ul);
-  for (let a = 0; a < Images.all.length; a++) {
-    let li = document.createElement('li');
-    ul.appendChild(li);
-    li.textContent = `${Images.all[a].name} had a ${Images.all[a].clicks} votes and was seen a ${Images.all[a].shown}`;
-  }
+/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+section.addEventListener('click',show);
+function show(e){if(display<25){
+  if(e.target.id==='leftImage'){renderImages();display++;Images.allImages[leftRandom].click++;}
+  if(e.target.id==='middleImage'){renderImages();display++;Images.allImages[middleRandom].click++;}
+  if(e.target.id==='rightImage'){renderImages();display++;Images.allImages[rightRandom].click++;}
+}else{button.addEventListener('click',showResults);section.removeEventListener('click',show); renderChart();localStorage.setItem('images',JSON.stringify(Images.allImages));}}
+/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.*/
+function showResults(){for(let a=0;a<Images.allImages.length;a++){
+    const li=document.createElement('li');
+li.textContent=`${Images.allImages[a].name} had a ${Images.allImages[a].click} votes and was seen a ${Images.allImages[a].shown}`;
+ul.appendChild(li);
+}button.removeEventListener('click',showResults);
 }
+/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
 function renderChart(){
-  let _clicks=[];
-  let _names=[];
-  let _showes=[];
-  for(let b=0;b<Images.all.length;b++){_clicks.push(Images.all[b].clicks);_names.push(Images.all[b].name);_showes.push(Images.all[b].shown);}
+    let _names=[];
+    let _click=[];
+    let _shown=[];
+    for(let x=0;x<Images.allImages.length;x++){_names.push(Images.allImages[x].name); _click.push(Images.allImages[x].click); _shown.push(Images.allImages[x].shown);}
+    const chartContainer=document.getElementById('chart').getContext('2d');
+    let myChart=new Chart(chartContainer,{
+type:'bar',
+data:{labels:_names,
+datasets:[
+    {label:'# of clicks',
+    data:_click,
+    backgroundColor:'red',
+    borderColor:'blue',
+    borderWidth:1},
 
-  let ctx = document.getElementById('myChart').getContext('2d');
-  // eslint-disable-next-line no-unused-vars
-  let myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels:_names,
-      datasets: [{
-        label: '# of Votes',
-        data: _clicks,
-        backgroundColor:
-        'rgba(255, 99, 132, 1)',
-        borderColor:
-          'rgba(255, 99, 132, 1)',
-        borderWidth: 1},
-
-
-      {
-
-        label: '3 of shoes',
-        data: _showes,
-        backgroundColor:
-            'rgba(54, 162, 235, 0.2)',
-
-        borderColor:
-        'rgba(54, 162, 235, 0.2)',
-        borderWidth: 1
-
-      }
-      ]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
+    {label:'# of shownes',
+        data:_shown,
+        backgroundColor:'yellow',
+        borderColor:'green',
+        borderWidth:1}
+]
+},
+options:{
+    scales:{
+        y:{
+            beginAtZero:true}
     }
-  }
-  );
 }
 
-function getData(){
-  let data=JSON.parse(localStorage.getItem('key'));
+    });
 
-  if(data){
-    for(let a=0;a<data.length;a++){
-      new Images(data[a].name);
-    }
-    renderImages();
-  }
 }
+/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
